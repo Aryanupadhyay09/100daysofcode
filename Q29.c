25a9b3da65e2d7541585a369751abd1561a4ddac
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-10-31 02:06:36 +0530

    Add factorial calculation program in Q29.c
    This program prompts the user for a number and calculates its factorial using a loop.

---
+++ Q29.c
@@ -0,0 +1,14 @@
+Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main() {
+    int n;
+    unsigned long long fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+
+    printf("Factorial = %llu\n", fact);
+    return 0;
+}
