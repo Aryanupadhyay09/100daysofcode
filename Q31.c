cef630bc26e7b3107efc94f2d36534655d82a968
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-06 19:25:26 +0530

    Implement binary conversion for a given number

---
+++ Q31.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, binary[32], i = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    while (num > 0) {
+        binary[i++] = num % 2;
+        num /= 2;
+    }
+
+    printf("Binary: ");
+    while (i--)
+        printf("%d", binary[i]);
+
+    return 0;
+}
