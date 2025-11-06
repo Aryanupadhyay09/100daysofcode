57aabfc93bd2da6b706c5cb8384c50f6d80f9c7b
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-06 19:31:59 +0530

    Add program to check for perfect numbers

---
+++ Q42.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i < num; i++) {
+        if (num % i == 0)
+            sum += i;
+    }
+
+    if (sum == num)
+        printf("Perfect number\n");
+    else
+        printf("Not a perfect number\n");
+
+    return 0;
+}
