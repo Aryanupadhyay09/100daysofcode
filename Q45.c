6f9476baeae7f3074976006917a2dc13b9aca007
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-06 19:33:35 +0530

    Create Q45.c

---
+++ Q45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += (2 * i) / (float)(4 * i - 1);
+    }
+
+    printf("Sum of series: %.2f\n", sum);
+    return 0;
+}
