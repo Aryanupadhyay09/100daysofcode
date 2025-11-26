3a3c283e9508512c516a4be26d426a2f3bcf1a67
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-26 23:44:05 +0530

    Add functionality to insert into sorted array
    Implement insertion of a number into a sorted array

---
+++ Q66.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, i, x;
+    scanf("%d", &n);
+    int a[n+1];
+    for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &x);
+    for (i = n-1; i >= 0 && a[i] > x; i--) a[i+1] = a[i];
+    a[i+1] = x;
+    for (i = 0; i <= n; i++) printf("%d ", a[i]);
+    return 0;
+}
