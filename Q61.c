184295d4a62ce8a9ff14e6fdb0e10ef745839175
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-26 23:39:59 +0530

    Create Q61.c

---
+++ Q61.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, key, found = 0;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &key);
+    for (int i = 0; i < n; i++)
+        if (a[i] == key) { found = 1; break; }
+    printf(found ? "Found" : "Not Found");
+    return 0;
+}
