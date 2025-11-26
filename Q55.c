2500c8a24102762636e2959830859f066a9bd1a8
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-26 23:37:02 +0530

    Implement prime number finder in Q55.c
    This program reads an integer n and prints all prime numbers up to n.

---
+++ Q55.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 2; i <= n; i++) {
+        int prime = 1;
+        for (int j = 2; j <= i/2; j++)
+            if (i % j == 0) prime = 0;
+        if (prime) printf("%d ", i);
+    }
+    return 0;
+}
