67544c4855d8863bceaadf5089af07db13c20a31
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-28 00:30:41 +0530

    Create Q113.c

---
+++ Q113.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cmp(const void*a,const void*b){ return (*(int*)a)-(*(int*)b); }
+int main(){
+    int n,k; if(scanf("%d %d",&n,&k)!=2) return 0;
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    qsort(a,n,sizeof(int),cmp);
+    printf("%d\n",a[k-1]);
+    return 0;
+}
