1729c836d978a529d1854f3d063e1e4d50e02e67
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-11-28 00:24:59 +0530

    Implement binary search for ceiling index

---
+++ Q102.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int ceil_index(int a[],int n,int x){
+    int l=0,r=n-1,ans=-1;
+    while(l<=r){
+        int m=(l+r)/2;
+        if(a[m]>=x){ ans=m; r=m-1; }
+        else l=m+1;
+    }
+    return ans;
+}
+int main(){
+    int n; if(scanf("%d",&n)!=1) return 0;
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    int x; scanf("%d",&x);
+    printf("%d\n",ceil_index(a,n,x));
+    return 0;
+}
